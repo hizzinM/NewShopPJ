304e87bab1aa8650b20fd075abf636fc8ca3ed2f
min <sia6693@naver.com>
2022-03-03 12:50:58 +0900

    기본설정

---
+++ New_Shop/src/test/java/com/newshop/persistence/JDBCTest.java
@@ -0,0 +1,32 @@
+package com.newshop.persistence;
+
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+public class JDBCTest {
+	 static { 
+	        try { 
+	            Class.forName("com.mysql.cj.jdbc.Driver"); 
+	        } catch(Exception e) { 
+	            e.printStackTrace(); 
+	        } 
+	    } 
+	    
+	    @Test 
+	    public void testConnection() { 
+	        try(Connection con = DriverManager.getConnection( 
+	                "jdbc:mysql://localhost:3306/newshop?serverTimezone=Asia/Seoul", 
+	                "root", 
+	                "mysql")){ 
+	            System.out.println(con); 
+	        } catch (Exception e) { 
+	            fail(e.getMessage()); 
+	        } 
+	    
+	    }    
+	        
+}
